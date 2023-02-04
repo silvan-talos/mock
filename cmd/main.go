06017d74edfd7031c9fca2052a91e66814272439
Silvan Taloș <silvan.talos@bobnet.ro>
2023-02-04 21:11:53 +0200

    Add basic CLI
    First sketch of a CLI. Processing will need pairs of an interface name
    that can be found at a certain file path. If one of them if missing the
    following assumptions are made:
    * if only file path is provided, then all the interfaces in that file
    are mocked
    * if only interface names are provided, then we will look for files
    containing the interface recurrently in the child directories

---
+++ cmd/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	ErrArgs = errors.New("please provide at least interface name or file path. Run `mock --help` for details")
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "mock",
+		Usage: "generate mocks for Go interfaces",
+		Authors: []*cli.Author{
+			{
+				Name:  "Silvan Talos",
+				Email: "silvantalos@ymail.com",
+			},
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "file",
+				Aliases: []string{"f"},
+				Usage:   "`PATH` of the file containing interface(s) to mock. If not defined, it will look in all .go files recurrently in child directories",
+			},
+			&cli.StringSliceFlag{
+				Name:    "interface",
+				Aliases: []string{"i"},
+				Usage:   "`NAME` of the interface(s) to mock. If not defined will mock all the interfaces in the file provided",
+			},
+		},
+		Action: startMocker,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startMocker(cCtx *cli.Context) error {
+	filePath := cCtx.String("file")
+	interfaces := cCtx.StringSlice("interface")
+	if filePath == "" && interfaces == nil {
+		return ErrArgs
+	}
+
+	return nil
+}
